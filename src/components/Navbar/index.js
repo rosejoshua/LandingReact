4b2c9e6db6f30649c76fed50540881b84497879f
Joshua Rose <jlr007@protonmail.com>
2021-12-11 12:31:57 -0500

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarConstainer>
+                    <NavLogo>
+                        dolla
+                    </NavLogo>
+                </NavbarConstainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
